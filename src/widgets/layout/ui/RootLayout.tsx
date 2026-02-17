abafeebac6413b949bdd9ccbdd8eee882dfeb607
Dobbymin <gmin990624@gmail.com>
2026-02-17 23:14:06 +0900

    feat: header와 footer 을 추가한 전체 layout 추가

--- src/widgets/layout/ui/RootLayout.tsx
+++ src/widgets/layout/ui/RootLayout.tsx
@@ -2,21 +2,24 @@ import { Outlet } from "react-router";
 
 import { Box, Flex } from "@chakra-ui/react";
 
-import { Header } from "../components";
+import { Footer, Header } from "../components";
 
 export const RootLayout = () => {
   return (
     <Flex
       direction="column"
-      maxW={{ base: "100vw", lg: "900px" }}
-      maxH={{ base: "100dvh", md: "750px" }}
+      maxW={{ base: "100vw", lg: "1280px" }}
+      maxH={{ base: "100dvh", md: "720px" }}
       w="100vw"
       h="100dvh"
       mx="auto"
     >
       <Header />
-      <Box w="full" as="main" flex={1}>
-        <Outlet />
+      <Box mx="auto" w="full" as="main">
+        <Box px={3} flex={1} w="full">
+          <Outlet />
+        </Box>
+        <Footer />
       </Box>
     </Flex>
   );
