393cb64140a9e7312ccc8ff5d488c4e9670baaf1
Dobbymin <gmin0701@knu.ac.kr>
2026-02-18 13:27:33 +0000

    chore: 로고 이미지 파일 추가

--- src/shared/theme/config/style-system-config.ts
+++ src/shared/theme/config/style-system-config.ts
@@ -1,12 +1,26 @@
 import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 
+import { colorSemanticToken, colorToken } from "../../styles";
 import { globalStyles } from "../global";
 
 const config = defineConfig({
   cssVarsRoot: ":where(:root, :host)",
-  cssVarsPrefix: "ck",
+  cssVarsPrefix: "sd",
 
   globalCss: globalStyles,
+
+  theme: {
+    tokens: {
+      fonts: {
+        heading: { value: "Pretendard" },
+        body: { value: "Pretendard" },
+      },
+      colors: colorToken,
+    },
+    semanticTokens: {
+      colors: colorSemanticToken,
+    },
+  },
 });
 
 export const system = createSystem(defaultConfig, config);
--- src/widgets/layout/ui/RootLayout.tsx
+++ src/widgets/layout/ui/RootLayout.tsx
@@ -2,22 +2,24 @@ import { Outlet } from "react-router";
 
 import { Box, Flex } from "@chakra-ui/react";
 
-import { Header } from "../components";
+import { Footer, Header } from "../components";
 
 export const RootLayout = () => {
   return (
-    <Flex
-      direction="column"
-      maxW={{ base: "100vw", lg: "900px" }}
-      maxH={{ base: "100dvh", md: "750px" }}
-      w="100vw"
-      h="100dvh"
-      mx="auto"
-    >
-      <Header />
-      <Box w="full" as="main" flex={1}>
-        <Outlet />
-      </Box>
+    <Flex direction="column" minH="100dvh">
+      <Flex
+        direction="column"
+        maxW={{ base: "100vw", lg: "1280px" }}
+        w="100vw"
+        mx="auto"
+        flex={1}
+      >
+        <Header />
+        <Box as="main" flex={1} w="full" mx="auto" p={3} overflowY="auto">
+          <Outlet />
+        </Box>
+      </Flex>
+      <Footer />
     </Flex>
   );
 };
