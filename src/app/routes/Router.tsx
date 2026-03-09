635de35d129cd3ae57e6e56d95cd45402754a189
Dobbymin <gmin990624@gmail.com>
2026-03-10 02:35:33 +0900

    feat(upload): 업로드 페이지 route path 추가

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,6 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 
-import { ErrorPage, MainPage, MyPage } from "@/pages";
+import {
+  ErrorPage,
+  MainPage,
+  MyPage,
+  UploadCompletePage,
+  UploadLoadingPage,
+  UploadPage,
+  UploadStepPage,
+} from "@/pages";
 import { ROUTE_PATHS } from "@/shared";
 import { RootLayout } from "@/widgets";
 
@@ -11,6 +19,16 @@ export const Router = () => {
         <Route element={<RootLayout />}>
           <Route path={ROUTE_PATHS.MAIN} element={<MainPage />} />
           <Route path={ROUTE_PATHS.MYPAGE} element={<MyPage />} />
+          <Route path={ROUTE_PATHS.FILE_UPLOAD} element={<UploadPage />} />
+          <Route
+            path={ROUTE_PATHS.UPLOAD_LOADING}
+            element={<UploadLoadingPage />}
+          />
+          <Route path={ROUTE_PATHS.UPLOAD_STEP} element={<UploadStepPage />} />
+          <Route
+            path={ROUTE_PATHS.UPLOAD_COMPLETE}
+            element={<UploadCompletePage />}
+          />
 
           <Route path={ROUTE_PATHS.NOT_FOUND} element={<ErrorPage />} />
         </Route>
