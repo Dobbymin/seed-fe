a9846d8684f671c759919e18d5328266cba51bac
Dobbymin <gmin990624@gmail.com>
2026-03-15 16:10:25 +0900

    feat(user): 사용자 정보 전역변수 저장 store 및 hook 구현

---
+++ src/entities/user/model/store/user-info.store.ts
@@ -0,0 +1,59 @@
+import { create } from "zustand";
+import {
+  combine,
+  createJSONStorage,
+  devtools,
+  persist,
+} from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+
+import type { UserInfoResponse } from "../apis";
+
+type PersistedUserProfile = Pick<UserInfoResponse, "nickname" | "profileUrl">;
+
+type UserInfoState = {
+  userInfo: UserInfoResponse | null;
+  persistedProfile: PersistedUserProfile | null;
+};
+
+type UserInfoActions = {
+  setUserInfo: (userInfo: UserInfoResponse) => void;
+  clearUserInfo: () => void;
+};
+
+const initialState: UserInfoState = {
+  userInfo: null,
+  persistedProfile: null,
+};
+
+export const useUserInfoStore = create<UserInfoState & UserInfoActions>()(
+  devtools(
+    persist(
+      immer(
+        combine(initialState, (set) => ({
+          setUserInfo: (userInfo: UserInfoResponse) =>
+            set((state) => {
+              state.userInfo = userInfo;
+              state.persistedProfile = {
+                nickname: userInfo.nickname,
+                profileUrl: userInfo.profileUrl,
+              };
+            }),
+          clearUserInfo: () =>
+            set((state) => {
+              state.userInfo = null;
+              state.persistedProfile = null;
+            }),
+        })),
+      ),
+      {
+        name: "user-info-store",
+        storage: createJSONStorage(() => sessionStorage),
+        partialize: (state) => ({ persistedProfile: state.persistedProfile }),
+      },
+    ),
+    {
+      name: "user-info-store-devtools",
+    },
+  ),
+);
